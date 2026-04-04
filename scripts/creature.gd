de74da7bc7ce77343a07ed3bd1d9536ea1d3e7fe
gwak-amole <gwaksophie@gmail.com>
2026-04-03 20:21:39 -0700

    character movement + tilemap set

---
+++ scripts/creature.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+@export var speed = 200.0
+
+func _physics_process(delta):
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	if direction:
+		$AnimatedSprite2D.play("walking")
+		if direction.x > 0:
+			$AnimatedSprite2D.flip_h = true
+		elif direction.x <0:
+			$AnimatedSprite2D.flip_h = false
+		velocity = direction * speed
+	else:
+		$AnimatedSprite2D.play("idle")
+		velocity = Vector2.ZERO
+	
+	move_and_slide()
+		
